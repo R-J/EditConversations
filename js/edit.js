4650a832ecd27b1d405761e34d55f30713b33c6f
R-J <robin.jurinka@gmx.net>
2013-08-23 10:18:08 +0200

    Update edit.js

--- js/edit.js
+++ js/edit.js
@@ -4,17 +4,38 @@ jQuery(document).ready(function($) {
          $(this).autogrow();
       });
 
-   $("a.EditMessage").click(function() {
-       var input = $("<textarea>", { val: $(this).text(),
-         cols: "100",
+   $("a.EditMessage").click(function(event) {
+      var elLink = this;
+      var elMsgdiv = $(this).parent().next();
+
+/* insecure, but if check for userid and unread is done, that's not an issue */      
+      var MessageID = elMsgdiv.get(0).id.substr(8, 10);
+      alert('MessageID = '  + MessageID + '.');
+
+      elMsgdiv = $(elMsgdiv).find('.Message');
+/*
+   msgtext must be called by ajax request
+   (where messageid = messageid
+    and userid = userid -- security: otherwise attacker might view any message
+    and message = unread -- comfort: stop user from senseless work as early as possible)
+   security: userid must not be spoofable!!!! otherwise attacker could see any conversation message!!!!
+ */      
+      var msgtext = elMsgdiv.text().trim();
+      var elInput = $("<textarea>", { val: msgtext,
          rows: "6",
-         class: "MessageBox",
-         style: "overflow: hidden; display: block;"
+         class: "MessageBox EditBox"
          });
-         
-      /* replace not this, but the next Message */
-      /* change "Edit" to "Save" or create New button or even create save button in plugin but hide it with css and show only now? */
-      $(this).replaceWith(input);
-      input.select();
+      elMsgdiv.replaceWith(elInput);
+      elInput.select();
+      elLink.innerHTML = 'Save';
+
+// rename "edit" to "save"
+// check if message is still unread and also that userid is the same!
+//    yes: save
+//    no: echo error
+// change textarea back to div, using the right formatter
+      
+      event.preventDefault();
+      return false;
    });
 });
